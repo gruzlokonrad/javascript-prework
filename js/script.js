ed9ad3bf395af734fcd2a244044af52b10b91470
Konrad Gruzlo <gruzlokonrad@gmail.com>
2022-06-17 09:18:51 +0200

    add if loop

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+  playerMove = 'kamień';
+} if(playerInput == '2'){
+  playerMove = 'papier';
+} if(playerInput == '3'){
+  playerMove = 'nożyce';
+} 
+
+printMessage('Twój ruch to: ' + playerMove);
